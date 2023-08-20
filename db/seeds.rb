79cddbf3935fa6b99229b3d5122b6a1576d0f414
Alejandro Piñango <alejandropinango@gmail.com>
2023-08-20 18:17:33 -0400

    Se agregó a la vista show y al controlador articles para que muestre los comentarios.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Creando registros'
+
+# seed.rb
+
+# 20.times do
+#   Article.create(
+#     title: Faker::Book.title,
+#     description: Faker::Lorem.sentence,
+#     content: Faker::Lorem.paragraphs.join("\n\n"),
+#     user_id: 1
+#   )
+# end
+
+# puts 'Registros creado en la bd'
+
+puts 'Creando comentarios'
+
+# Asociar un comentario a cada artículo
+Article.all.each do |article|
+  article.comments.create(
+    content: Faker::Lorem.sentence,
+    user_id: 1
+  )
+end
+
+puts 'Comentarios agregados'
